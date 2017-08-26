55734b771b30fd7fdf8b949392f52a6f9c8f05e0
Aaron Suarez <aaosuarez@gmail.com>
2017-08-26 10:54:46 -0700

    Convert MVP to class-based implementation

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+"use strict";
+
+function Remixer(options) {
+    this.config = this.mergeSettings(options);
+    this.selector = (typeof this.config.selector == "string") ? document.querySelector(this.config.selector) : this.config.selector;
+    this.init();
+}
+
+Remixer.prototype.mergeSettings = function(userSettings) {
+    var settings = {
+        selector: ".remixer"
+    };
+    for (var attribute in userSettings) {
+        settings[attribute] = userSettings[attribute]
+    }
+    return settings;
+}
+
+Remixer.prototype.init = function() {
+    if (this.selector === null) {
+        throw new Error("Selector not present or misconfigured.");
+    }
+    if (this.tracks === null) {
+        throw new Error("Track information not provided.");
+    }
+    this.selector.addEventListener("timeupdate", this.timeupdateHandler.bind(this));
+}
+
+Remixer.prototype.timeupdateHandler = function(e) {
+    var currentTime = e.srcElement.currentTime;
+    var currentTrack = this.getCurrentTrackFromTimestamp(currentTime);
+    this.setTrackInfo(currentTrack.title + " - " + currentTrack.artist);
+}
+
+Remixer.prototype.getCurrentTrackFromTimestamp = function(timestamp) {
+    var timestamps = this.config.tracks.map(function(track) {
+        return track.timestamp;
+    });
+    
+    var trackIndex = 0;
+    for (var i = 0; i < timestamps.length; i++) {
+        if (timestamp > timestamps[i]) {
+            trackIndex = i;
+        }
+    }
+    return this.config.tracks[trackIndex];
+}
+
+Remixer.prototype.setTrackInfo = function(text) {
+    var trackInfo = document.querySelector(".remixer__track-info");
+    trackInfo.textContent = text;
+}
+
 var tracks = [
     {
         "title": "You Can Do It (Jar Jar Kinx Remix)",
@@ -26,29 +79,6 @@ var tracks = [
     }
 ];
 
-var remixer = document.getElementById("remixer");
-
-remixer.addEventListener("timeupdate", function(e) {
-    var currentTime = e.srcElement.currentTime;
-    var currentTrack = getCurrentTrackFromTimestamp(currentTime);
-    setTrackInfo(currentTrack.title + " - " + currentTrack.artist);
+var r = new Remixer({
+    tracks: tracks
 });
-
-function getCurrentTrackFromTimestamp(timestamp) {
-    var timestamps = tracks.map(function(track) {
-        return track.timestamp;
-    });
-
-    var trackIndex = 0;
-    for (var i = 0; i < timestamps.length; i++) {
-        if (timestamp > timestamps[i]) {
-            trackIndex = i;
-        }
-    }
-    return tracks[trackIndex];
-}
-
-function setTrackInfo(text) {
-    var trackInfo = document.getElementsByClassName("remixer__track-info")[0];
-    trackInfo.textContent = text;
-}
